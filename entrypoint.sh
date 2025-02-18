cf90ca733d9290b26b3959e34bc1fbc0b7264dbc
Thibault Le Ouay <thibaultleouay@gmail.com>
2025-02-18 14:46:48 +0100

    🚀 improve entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh -l
 
-ls
+if [ -z "$INPUT_API_KEY" ]; then
+    /home/openstatus/openstatus run --access-token $INPUT_API_KEY
+else
+    /home/openstatus/openstatus run --access-token $INPUT_API_KEY --config $INPUT_CONFIG_PATH
+fi
 
-/home/openstatus/openstatus run --access-token $INPUT_API_KEY
-
-exit 0
+if [ $? -eq 0 ]
+then
+    echo "OpenStatus run successfully"
+    exit 0
+else
+    echo "OpenStatus run failed"
+    exit 1
+fi
